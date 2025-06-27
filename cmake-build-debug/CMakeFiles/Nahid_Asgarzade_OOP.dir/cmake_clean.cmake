1f7968c0077faf8f77d42414bfca7d4b5fa31a24
asgarzaden <asgarzadenahid@outlook.com>
2025-06-27 18:52:21 +0200

    Try

---
+++ cmake-build-debug/CMakeFiles/Nahid_Asgarzade_OOP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Nahid_Asgarzade_OOP.dir/main.cpp.obj"
+  "Nahid_Asgarzade_OOP.exe"
+  "Nahid_Asgarzade_OOP.exe.manifest"
+  "Nahid_Asgarzade_OOP.pdb"
+  "libNahid_Asgarzade_OOP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Nahid_Asgarzade_OOP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Nahid_Asgarzade_OOP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Nahid_Asgarzade_OOP.
+# This may be replaced when dependencies are built.
